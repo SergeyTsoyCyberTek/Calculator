4a9509cc339fa9d5db7c7c5c38f61f52be470ad4
SERGEY TSOY <stsoy08@gmail.com>
2019-01-19 11:34:56 -0800

    add comment to add method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-
+    //this is a very useful method.
     public static double add(double num1, double num2){
         return num1 + num2;
     }
@@ -7,4 +7,9 @@ public class Calculator {
     public static double subtract(double num1, double num2){
         return num1-num2;
     }
+
+    public static double multiply(double num1, double num2){
+        return num1 * num2;
+    }
+
 }
